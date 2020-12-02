1e1b2a5a54950c03f179dc5a4e793c7ea81d136e
tatoalo <apogliaghi@gmail.com>
2020-12-02 15:48:19 +0100

    Solved Day 2 🎄🎅🏻

--- AoC20/day2.py
+++ AoC20/day2.py
@@ -5,6 +5,7 @@ def read_input(f):
     return input_lines
 
 
+# Old Password Policy
 def extrapolate_data(d):
     valid = 0
     for item in d:
@@ -28,6 +29,27 @@ def extrapolate_data(d):
     return valid
 
 
+# New Password Policy
+def extrapolate_data_part_2(d):
+    valid = 0
+    for item in d:
+        split_item = item.split(' ')
+
+        lowerB, upperB = split_item[0].split('-')
+        lowerB = int(lowerB)
+        upperB = int(upperB)
+        target = split_item[1].split(':')[0]
+        psw = split_item[2]
+
+        lowerTest = psw[lowerB-1] == target
+        upperTest = psw[upperB-1] == target
+
+        if lowerTest + upperTest == 1:
+            valid += 1
+
+    return valid
+
+
 def main():
     DEBUG = False
     if DEBUG:
@@ -35,7 +57,8 @@ def main():
     else:
         data = read_input("data2")
 
-    print(f"Valid passwords: {extrapolate_data(data)}")
+    print(f"Valid OLD POLICY passwords: {extrapolate_data(data)}")
+    print(f"Valid NEW POLICY passwords: {extrapolate_data_part_2(data)}")
 
 
 if __name__ == "__main__":
